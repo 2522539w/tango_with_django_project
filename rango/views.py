cf7da5c62b1aec7bbe0d909e675c2192db19f1fa
2522539w <2522539w@student.gla.ac.uk>
2021-07-27 14:00:46 +0100

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -15,26 +15,19 @@ def index(request):
 def about(request):
     return render(request, 'rango/about.html')
 def show_category(request, category_name_slug):
-# 创建上下文字典，稍后传给模板渲染引擎
     context_dict = {}
     try:
-# 能通过传入的分类别名找到对应的分类吗？
-# 如果找不到，.get() 方法抛出 DoesNotExist 异常
-# 因此 .get() 方法返回一个模型实例或抛出异常
+
         category = Category.objects.get(slug=category_name_slug)
-# 检索关联的所有网页
-# 注意，filter() 返回一个网页对象列表或空列表
+
         pages = Page.objects.filter(category=category)
-# 把得到的列表赋值给模板上下文中名为 pages 的键
+
         context_dict['pages'] = pages
-# 也把从数据库中获取的 category 对象添加到上下文字典中
-# 我们将在模板中通过这个变量确认分类是否存在
+
         context_dict['category'] = category
     except Category.DoesNotExist:
-# 没找到指定的分类时执行这里
-# 什么也不做
-# 模板会显示消息，指明分类不存在
+
         context_dict['category'] = None
         context_dict['pages'] = None
-# 渲染响应，返回给客户端
+
     return render(request, 'rango/category.html', context_dict)
